c4e4a5b9d4e8d7686126cce1b83cceab490d0a09
ERICA CORBI <corbi.erica@gmail.com>
2020-12-05 10:08:42 +0100

    Load Heatmap

--- components/app/functionsApp.js
+++ components/app/functionsApp.js
@@ -47,5 +47,7 @@ const getCrimesTypologies = () => {
   return crimes;
 
  }
+ 
+
 
 
--- components/map/functionsMap.js
+++ components/map/functionsMap.js
@@ -1,8 +1,8 @@
 
 // Map Component State Object
 var mapState = {
-    "hotels" : [],
-    "crimes" : [],
+    "hotels": [],
+    "crimes": [],
 }
 
 var MAP;
@@ -10,20 +10,21 @@ var MAP;
 /**
  * This starts the map component by building it and taking data for map
  */
-const startMapComponent = () =>{
+const startMapComponent = () => {
 
 
     // Build html
-   // _buildMapSection();
+    // _buildMapSection();
 
     // Load Map
 
     _loadMap();
 
     _loadHotels();
+   
+    _loadHeatMap();
+  
 
-
-    // Print data
 
     console.log("Avvia la mappa");
 }
@@ -57,29 +58,30 @@ const _loadMap = () => {
     MAP = new L.Map('map_placeholder');
     let url = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
     let attrib = 'Map data (c)OpenStreetMap contributors';
-    let osm = new L.TileLayer(url, {minZoom: 8, maxZoom: 16, attribution: attrib});
-    MAP.setView(new L.LatLng(41.85,-87.65),12);
+    let osm = new L.TileLayer(url, { minZoom: 8, maxZoom: 16, attribution: attrib });
+    MAP.setView(new L.LatLng(41.85, -87.65), 11);
     MAP.addLayer(osm);
+
 
 }
 
 const _loadHotels = () => {
 
     mapState.hotels = [];
-    
+
     // let lati = getLatitude();
     // let longi = getLongitude();
     // let nome = getName();
 
-    for(let i = 0; i < HOTELS.data.length; i++){
+    for (let i = 0; i < HOTELS.data.length; i++) {
 
         let lat = HOTELS.data[i][36];
         let lon = HOTELS.data[i][37];
         let name = HOTELS.data[i][12];
 
         let stars = getRandomStars(lat, lon);
-        
-        if(lat != null && lon != null){
+
+        if (lat != null && lon != null) {
 
             mapState.hotels.push({
                 latitude: lat,
@@ -87,22 +89,46 @@ const _loadHotels = () => {
                 name: name,
                 stars: stars,
             });
-            
+
             L.marker([lat, lon]).addTo(MAP).bindPopup(name + "\n" + "stelle:" + stars);
         }
 
     }
-    
+
 }
 
+const _loadHeatMap = () => {
+
+    let osmLayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data (c)OpenStreetMap contributors'
+    }).addTo(MAP);
+
+    crimes = appState.crimes;
+    let points = []
+    crimes.forEach(d => {
+        if (isNaN(parseFloat(d.longitude)) || isNaN(parseFloat(d.latitude))) {
+            return
+        }
+        else {
+            points.push([d.latitude, d.longitude,10])
+        }
+    })
+
+    let heat = L.heatLayer(points,{gradient:{0.4: 'yellow', 0.65: 'orange', 1: 'red'} }).addTo(MAP);
+}
+
+
+
+
+
 function getRandomStars(lat, lon) {
-    
+
     // TODO: generate random values based on distance with center of chicago
-    
+
     min = Math.ceil(1);
     max = Math.floor(5);
     return Math.floor(Math.random() * (max - min) + min);
-  }
+}
 
 // const getHotelField = (index) => {
 //     let datas=[];
